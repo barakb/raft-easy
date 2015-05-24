62fc18288b0f43b1f01dc4fbf9d82711567ac44f
Barak Bar Orion <barak.bar@gmail.com>
2015-05-24 10:01:46 +0300

    Initial commit

---
+++ networking/src/main/java/com/github/barakb/raft/net/Foo.java
@@ -0,0 +1,11 @@
+package com.github.barakb.raft.net;
+
+/**
+ * Created by Barak Bar Orion
+ * 5/24/15.
+ */
+public class Foo {
+    public static void main(String[] args) {
+        System.out.println("foo");
+    }
+}
